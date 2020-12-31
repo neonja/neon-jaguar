3ef267c080a31f8c4b46532e8b3506922d93ba54
alexClifton <oooalexoooo@gmail.com>
2020-12-30 23:05:17 -0800

    ready to sync with backend on get points

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,11 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Board({board}) {
   const { brand, item, id } = board;
-  
+  const { ourData, setOurData} = useState(42)
+
+  useEffect((() => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"; // this gets around cors errors
+    const url = "https://pacific-atoll-67065.herokuapp.com/";
+    fetch(proxyurl + url)
+      .then(x => x.text()) // this is needed until we change response to json
+      .then((x) => {
+        console.log('here is our stuff', ourData)
+      })
+      .catch(x => console.log('!!! x failed',x))
+  }) ,[])
+
   useEffect(() => {
-    console.log('!!!', board);
-  },[])
+    console.log('!!! current data is ---   ', ourData)
+  },[ourData])
 
   return (
     <div className="board">
